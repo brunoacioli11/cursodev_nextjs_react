2e07fd87461a06dd68081feafab7aaafb44c6df5
Bruno Lins <brunoaraujoacioli@hotmail.com>
2026-03-21 12:40:10 +0100

    add prettier and auto formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <h1>Pague o preço</h1>
-  );
+  return <h1>Pague o preço</h1>;
 }
--- app/projects/page.js
+++ app/projects/page.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Pague o preço</h1>
+  return <h1>Pague o preço</h1>;
 }
 
-export default Home
+export default Home;
