9c5f1e4cd4eb75f8f6be683b02bdae57cd777338
Bruno Lins <brunoaraujoacioli@hotmail.com>
2026-05-16 17:16:16 +0200

    add`database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,8 @@
-export default function status(request, response) {
-  response.status(200).json({ "message": "Alunos do curso.dev são pessoas acima da média"});
+import database from "../../../../infra/database";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log("oi");
+  console.log(result.rows[0].sum);
+  response.status(200).json({ "message": "Alunos do curso.dev são pessoas acima da média" });
 }
