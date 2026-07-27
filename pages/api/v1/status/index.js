0d4f996396be43e91cb33339e3a2133984ef1c5e
Bruno Lins <brunoaraujoacioli@hotmail.com>
2026-07-27 20:50:55 +0100

    Finish Status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,20 @@
 import database from "infra/database";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log("oi");
-  console.log(result.rows[0].sum);
-  response.status(200).json({ "message": "Alunos do curso.dev são pessoas acima da média" });
+  const databaseVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const openedConnections = await database.query('SELECT COUNT(*)::int FROM pg_stat_activity where datname = \'local_db\';');
+  
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: openedConnections.rows[0].count,
+      }
+    }
+  });
 }
