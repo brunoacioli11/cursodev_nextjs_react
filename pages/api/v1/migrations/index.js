3adfe72dfeb728127d54f36835eac73782f410d2
Bruno Lins <brunoaraujoacioli@hotmail.com>
2026-08-15 12:06:33 +0100

    Fix `/migrations`endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,20 +3,26 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptions = {
-    databaseUrl: dbClient,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    migrationsTable: "teste",
-    dryRun: true,
-    verbose: true,
-  };
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
+  }
+  let dbClient
 
   try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationOptions = {
+      databaseUrl: dbClient,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      migrationsTable: "teste",
+      dryRun: true,
+      verbose: true,
+    };
     if (request.method === 'GET') {
       const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-      await dbClient.end();
       return response.status(200).json(pendingMigrations);
     }
 
@@ -26,18 +32,14 @@ export default async function migrations(request, response) {
         dryRun: false,
       });
 
-      await dbClient.end();
-
       if (migratedMigrations.length > 0) {
         return response.status(201).json(migratedMigrations);
       }
       return response.status(200).json(migratedMigrations);
     }
-
   } catch (error) {
     console.error(error);
   } finally {
     await dbClient.end();
-    return response.status(405).end();
   }
 }
