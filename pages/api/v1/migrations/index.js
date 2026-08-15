abdc4fdb2e2a149b681eb027ca358a09a73b5782
Bruno Lins <brunoaraujoacioli@hotmail.com>
2026-08-15 11:06:15 +0100

    Fix opened connections when METHOD is not POST or GET

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -13,25 +13,31 @@ export default async function migrations(request, response) {
     verbose: true,
   };
 
-  if (request.method === 'GET') {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
-  }
-
-  if (request.method === 'POST') {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
-
-    await dbClient.end();
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+  try {
+    if (request.method === 'GET') {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      await dbClient.end();
+      return response.status(200).json(pendingMigrations);
     }
-    return response.status(200).json(migratedMigrations);
-  }
 
-  return response.status(405).end();
+    if (request.method === 'POST') {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+
+      await dbClient.end();
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+      return response.status(200).json(migratedMigrations);
+    }
+
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await dbClient.end();
+    return response.status(405).end();
+  }
 }
