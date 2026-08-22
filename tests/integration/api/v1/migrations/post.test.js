e68b10d3ff377b0bee08cfd65790a7917cb69a1f
Bruno Lins <brunoaraujoacioli@hotmail.com>
2026-08-22 11:40:17 +0100

    makes > cursodev_nextjs_react@0.1.0 test > npm run services:up && npm run wait-for-postgres && concurrently --names next,jest --hide next --kill-others --success command-jest 'next dev' 'jest --runInBand'
    > cursodev_nextjs_react@0.1.0 services:up
    > docker compose -f infra/compose.yaml up -d
    
    > cursodev_nextjs_react@0.1.0 wait-for-postgres
    > node infra/scripts/wait-for-postgres.js
    
    🔴 Aguardando postgres aceitar conexões
    🟢 Postgres está pronto e aceitando conexões!
    
    [jest] ◇ injected env (6) from .env.development // tip: ⌘ custom filepath { path: '/custom/path/.env' }
    [jest] PASS tests/integration/api/v1/status/get.test.js
    [jest] PASS tests/integration/api/v1/migrations/post.test.js
    [jest] PASS tests/integration/api/v1/migrations/get.test.js
    [jest]   ● Console
    [jest]
    [jest]     console.log
    [jest]       [
    [jest]         {
    [jest]           path: '/Users/brunolins/cursoDev/challengeNextJSReact/cursodev_nextjs_react/infra/migrations/1785702356982_first-migration-test.js',
    [jest]           name: '1785702356982_first-migration-test',
    [jest]           timestamp: 1785702356982
    [jest]         },
    [jest]         {
    [jest]           path: '/Users/brunolins/cursoDev/challengeNextJSReact/cursodev_nextjs_react/infra/migrations/1785702360363_second-migration-test.js',
    [jest]           name: '1785702360363_second-migration-test',
    [jest]           timestamp: 1785702360363
    [jest]         }
    [jest]       ]
    [jest]
    [jest]       at Object.log (tests/integration/api/v1/migrations/get.test.js:15:11)
    [jest]
    [jest]
    [jest] Test Suites: 3 passed, 3 total
    [jest] Tests:       3 passed, 3 total
    [jest] Snapshots:   0 total
    [jest] Time:        0.668 s, estimated 1 s
    [jest] Ran all test suites.
    [jest] jest --runInBand exited with code 0
    --> Sending SIGTERM to other processes..more robust with  and

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,11 +1,10 @@
 import database from 'infra/database.js';
+import orchestrator from "tests/orchestrator.js";
 
-
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("DROP schema public cascade; CREATE schema public;");
-}
+});
 
 test("POST to /api/v1/migrations should return 200", async () => {
   const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
@@ -26,7 +25,7 @@ test("POST to /api/v1/migrations should return 200", async () => {
 
   const response2Body = await response2.json();
 
-  
+
 
   expect(Array.isArray(response2Body)).toBe(true);
   expect(response2Body.length).toBe(0);
