427614ff3c3337080653acba4a3c12c9ec99c7d2
Bruno Lins <brunoaraujoacioli@hotmail.com>
2026-08-29 15:56:33 +0100

    Add `lint:eslint:check` script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
+import js from "@eslint/js";
+import jest from "eslint-plugin-jest";
+import prettier from "eslint-config-prettier/flat";
 import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
+  js.configs.recommended,
   ...nextVitals,
-  ...nextTs,
+  prettier,
+  //...nextTs,
+  {
+    files: ["**/*.test.js"],
+    ...jest.configs["flat/recommended"],
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
